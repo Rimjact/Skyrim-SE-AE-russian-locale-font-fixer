c76219d68781f7c4c490a8f4b0a1f16e29f87fd3
Kirill <Rimjact@ya.ru>
2024-03-04 01:14:58 +0300

    Updated source code & release

---
+++ SkyrimRuLocaleFixer/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace SkyrimRuLocaleFixer
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.FixButton = new System.Windows.Forms.Button();
+            this.BackToDefaultButton = new System.Windows.Forms.Button();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // FixButton
+            // 
+            this.FixButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FixButton.Location = new System.Drawing.Point(12, 12);
+            this.FixButton.Name = "FixButton";
+            this.FixButton.Size = new System.Drawing.Size(344, 45);
+            this.FixButton.TabIndex = 0;
+            this.FixButton.Text = "Исправить шрифт русской локализации";
+            this.FixButton.UseVisualStyleBackColor = true;
+            this.FixButton.Click += new System.EventHandler(this.FixButton_Click);
+            // 
+            // BackToDefaultButton
+            // 
+            this.BackToDefaultButton.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BackToDefaultButton.Location = new System.Drawing.Point(12, 63);
+            this.BackToDefaultButton.Name = "BackToDefaultButton";
+            this.BackToDefaultButton.Size = new System.Drawing.Size(344, 45);
+            this.BackToDefaultButton.TabIndex = 1;
+            this.BackToDefaultButton.Text = "Вернуть по умолчанию";
+            this.BackToDefaultButton.UseVisualStyleBackColor = true;
+            this.BackToDefaultButton.Click += new System.EventHandler(this.BackToDefaultButton_Click);
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.StatusLabel.Font = new System.Drawing.Font("Arial", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.StatusLabel.Location = new System.Drawing.Point(12, 116);
+            this.StatusLabel.Margin = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(344, 71);
+            this.StatusLabel.TabIndex = 2;
+            this.StatusLabel.Text = "Status";
+            this.StatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(12, 200);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 5, 0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(344, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "v1.0 (10.02.2024) | © Rimjact, 2024";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(367, 234);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.StatusLabel);
+            this.Controls.Add(this.BackToDefaultButton);
+            this.Controls.Add(this.FixButton);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Исправитель шрифта русской локализации Skyrim SE";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button FixButton;
+        private System.Windows.Forms.Button BackToDefaultButton;
+        private System.Windows.Forms.Label StatusLabel;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SkyrimRuLocaleFixer/MainForm.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+using System.Security.Permissions;
+using System.IO.Pipes;
+using System.Linq;
+using System.Drawing;
+
+namespace SkyrimRuLocaleFixer
+{
+    public partial class MainForm : Form
+    {
+        private enum FormButtonType
+        {
+            All = 0,
+            Fix = 1,
+            BackToDefault = 2 
+        }
+
+        /// <summary>
+        /// Хранит имя пользователя компьютера для доступа к папке Documents
+        /// </summary>
+        private string _userName = string.Empty;
+        /// <summary>
+        /// Хранит путь к файлу Skyrim.ini
+        /// </summary>
+        private string _skyrimIniFilePath = string.Empty;
+        /// <summary>
+        /// Хранит путь к файлу Skyrim_backup.ini
+        /// </summary>
+        private string _skyrimIniBackupFilePath = string.Empty;
+        /// <summary>
+        /// Хранит строку исправления шрифтра русской локализации для Skyrim.ini
+        /// </summary>
+        private string _fixString = @"[Fonts]
+sFontConfigFile=Interface\FontConfig_ru.txt";
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            // Получаем имя пользователя
+            _userName = Environment.UserName;
+            _skyrimIniFilePath = $@"c:\Users\{_userName}\Documents\My Games\Skyrim Special Edition\Skyrim.ini";
+            _skyrimIniBackupFilePath = $@"c:\Users\{_userName}\Documents\My Games\Skyrim Special Edition\Skyrim_backup.ini";
+
+            CheckSkyrimIniFile();
+        }
+
+        /// <summary>
+        /// Обновляет текст и его цвет на StatusLabel элементе формы.
+        /// </summary>
+        /// <param name="statusText">Текст статуса</param>
+        /// <param name="statusColor">Цвет статуса</param>
+        private void UpdateStatusLable(string statusText, Color statusColor)
+        {
+            StatusLabel.Text = $"——————— СТАТУС ———————\n{statusText}";
+            StatusLabel.ForeColor = statusColor;
+        }
+
+        /// <summary>
+        /// Блокирует/разблокирует выбранную кнопку (или все).
+        /// </summary>
+        /// <param name="button">Кнопка: All, Fix, ToDefault</param>
+        private void SetEnabledForButton(FormButtonType button, bool enabled)
+        {
+            switch (button)
+            {
+                case FormButtonType.All:
+                    FixButton.Enabled = enabled;
+                    BackToDefaultButton.Enabled = enabled;
+                    break;
+                case FormButtonType.Fix:
+                    FixButton.Enabled = enabled;
+                    break;
+                case FormButtonType.BackToDefault:
+                    BackToDefaultButton.Enabled = enabled;
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Выполняет проверки при старте програмы.
+        /// </summary>
+        private void CheckSkyrimIniFile()
+        {
+            // Проверяем наличие файла Skyrim.ini
+            if (!IsSkyrimIniFileExist())
+            {
+                UpdateStatusLable($"Ошибка! Файл Skyrim.ini не найден по пути:\n{_skyrimIniFilePath}", Color.DarkRed);
+                SetEnabledForButton(FormButtonType.All, false);
+                return;
+            }
+
+            // Проверяем возможность редактирования файла Skyrim.ini
+            if (!IsProgramCanEditiSkyrimIniFile())
+            {
+                UpdateStatusLable($"Ошибка! Программа не может редактировать\nфайл Skyrim.ini по пути:\n{_skyrimIniFilePath}.", Color.DarkRed);
+                SetEnabledForButton(FormButtonType.All, false);
+                return;
+            }
+
+            // Проверяем наличие исправления в файле Skyrim.ini
+            if (IsFixAlredyExistInSkyrimIniFile())
+            {
+                UpdateStatusLable($"В файле Skyrim.ini уже содержится исправление\n шрифта русской локализации.", Color.DarkGreen);
+                SetEnabledForButton(FormButtonType.Fix, false);
+                return;
+            }
+            else
+            {
+                UpdateStatusLable($"В файле Skyrim.ini отсутствует исправление шрифта русской локализации.", Color.BlueViolet);
+                SetEnabledForButton(FormButtonType.BackToDefault, false);
+                return;
+            }
+        }
+
+        /// <summary>
+        /// Метод, вызываемый при нажатии на кнопку исправления.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void FixButton_Click(object sender, EventArgs e)
+        {
+            // Создаём файл бэкапа перед изменением файла
+            CreateBackupSkyrimIniFile();
+
+            // Добавляем текст исправления в начало файла Skyrim.ini
+            var oldLines = File.ReadAllText(_skyrimIniFilePath);
+            var newLines = $"{oldLines}\n{_fixString}";
+            File.WriteAllText(_skyrimIniFilePath, newLines);
+
+            UpdateStatusLable($"Исправление шрифта русской локализации\nуспешно добавлено в Skyrim.ini.\nSkyrim_backup.ini создан в той же папке.", Color.DarkGreen);
+
+            SetEnabledForButton(FormButtonType.Fix, false);
+            SetEnabledForButton(FormButtonType.BackToDefault, true);
+        }
+        
+        /// <summary>
+        /// Метод, вызываемый при нажатии на кнопку возвращения поумолчанию.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BackToDefaultButton_Click(object sender, EventArgs e)
+        {
+            // Создаём файл бэкапа перед изменением файла
+            CreateBackupSkyrimIniFile();
+
+            // Заменяем текст исправления на пустую строку в файле Skyrim.ini
+            var oldLines = File.ReadAllLines(_skyrimIniFilePath);
+            var newLines = oldLines.Where(line => !line.Contains("[Fonts]")).Where(line => !line.Contains(@"sFontConfigFile=Interface\FontConfig_ru.txt"));
+            File.WriteAllLines(_skyrimIniFilePath, newLines);
+
+            UpdateStatusLable($"Исправление шрифта русской локализации\nуспешно удалено из Skyrim.ini!\nSkyrim_backup.ini создан в той же папке.", Color.BlueViolet);
+
+            SetEnabledForButton(FormButtonType.Fix, true);
+            SetEnabledForButton(FormButtonType.BackToDefault, false);
+        }
+
+        /// <summary>
+        /// Проверяет существует ли Skyrim.ini файл в документах пользователя.
+        /// </summary>
+        /// <returns>true/false</returns>
+        private bool IsSkyrimIniFileExist()
+        {
+            return File.Exists(_skyrimIniFilePath);
+        }
+
+        /// <summary>
+        /// Проверяет может ли програма изменять файл Skyrim.ini 
+        /// </summary>
+        /// <returns>true/false</returns>
+        private bool IsProgramCanEditiSkyrimIniFile()
+        {
+            // Устанавливает разрешение на запись для файла Skyrim.ini
+            var permissionSet = new PermissionSet(PermissionState.None);
+            var writePermission = new FileIOPermission(FileIOPermissionAccess.Write, _skyrimIniFilePath);
+            permissionSet.AddPermission(writePermission);
+
+            return permissionSet.IsSubsetOf(AppDomain.CurrentDomain.PermissionSet);
+        }
+
+        /// <summary>
+        /// Проверяет существует ли исправление шрифта в файле Skyrim.ini для избежания ошибок
+        /// </summary>
+        /// <returns>true/false</returns>
+        private bool IsFixAlredyExistInSkyrimIniFile()
+        {
+            using (FileStream fileStream = new FileStream(_skyrimIniFilePath, FileMode.Open))
+            using (TextReader reader = new StreamReader(fileStream)) 
+            {
+                return reader.ReadToEnd().Contains(_fixString);   
+            }
+        }
+
+        /// <summary>
+        /// Создаёт резеврную копию Skyrim.ini с названием Skyrim_backup.ini в той же папке
+        /// </summary>
+        private void CreateBackupSkyrimIniFile()
+        {
+            // Перезаписывать файл через File.Copy не разрешено, по этому просто удаляем старый файл бэкапа
+            if (File.Exists(_skyrimIniBackupFilePath))
+                File.Delete(_skyrimIniBackupFilePath);
+
+            // Создаём файл бэкапа путём копирования изначального Skyrim.ini
+            File.Copy(_skyrimIniFilePath, _skyrimIniBackupFilePath);
+        }
+    }
+}
